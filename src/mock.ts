62c183b14ba81afaeba9db81073669d07b15c92c
Nitei <44502403+Nitei@users.noreply.github.com>
2023-09-23 09:56:57 +0200

    1 file modified

--- src/mock.ts
+++ src/mock.ts
@@ -1,41 +1,32 @@
 import { Gasto } from './main';
 
 export const mock = {
-  presupuesto: '2200',
-  name: '',
-  amount: '',
-  sumatoria: 1900,
-  queda: 300,
-  gastos: [
-    {
-      name: 'mama',
-      amount: '200',
-      countIt: true,
-    } as Gasto,
-    {
-      name: 'hacienda',
-      amount: '150',
-      countIt: true,
-    } as Gasto,
-    {
-      name: 'cara',
-      amount: '150',
-      countIt: true,
-    } as Gasto,
-    {
-      name: 'piso',
-      amount: '700',
-      countIt: true,
-    } as Gasto,
-    {
-      name: 'muela',
-      amount: '400',
-      countIt: true,
-    } as Gasto,
-    {
-      name: 'gastos',
-      amount: '300',
-      countIt: true,
-    } as Gasto,
-  ],
-};
+  "presupuesto": "2200",
+  "name": "",
+  "amount": "",
+  "sumatoria": 1700,
+  "queda": 500,
+  "gastos": [
+      {
+          "name": "ma",
+          "amount": "200",
+          "countIt": true
+      },
+      {
+          "name": "haci",
+          "amount": "150",
+          "countIt": true
+      },
+      {
+          "name": "cara",
+          "amount": "150",
+          "countIt": true
+      },
+      {
+          "name": "gastos",
+          "amount": "500",
+          "countIt": true
+      },
+
+  ]
+}
